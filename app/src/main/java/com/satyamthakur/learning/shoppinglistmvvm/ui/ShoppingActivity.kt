b8e8894f6a03f7d223e56f0eeb9cf654f9d92720
Satyam Thakur <sat.thakur20@gmail.com>
2023-06-17 12:53:41 +0530

    Added ShoppingViewModelFactory class

--- app/src/main/java/com/satyamthakur/learning/shoppinglistmvvm/ui/ShoppingActivity.kt
+++ app/src/main/java/com/satyamthakur/learning/shoppinglistmvvm/ui/ShoppingActivity.kt
@@ -2,11 +2,22 @@ package com.satyamthakur.learning.shoppinglistmvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 import com.satyamthakur.learning.shoppinglistmvvm.R
+import com.satyamthakur.learning.shoppinglistmvvm.database.ShoppingDatabase
+import com.satyamthakur.learning.shoppinglistmvvm.repositories.ShoppingRepository
+import com.satyamthakur.learning.shoppinglistmvvm.ui.shoppinglist.ShoppingViewModel
+import com.satyamthakur.learning.shoppinglistmvvm.ui.shoppinglist.ShoppingViewModelFactory
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
+
+        val database = ShoppingDatabase(this)
+        val repository = ShoppingRepository(database)
+        val factory = ShoppingViewModelFactory(repository)
+
+        val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
     }
 }
